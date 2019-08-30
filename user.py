dc416ba8ffa1d33fd05cb52df34c16d355310fd8
Paulwamaria <paulwamaria@gmail.com>
2019-08-30 17:58:48 +0300

    Add test for saving multiple details

--- user.py
+++ user.py
@@ -1,11 +1,12 @@
 class User:
   user_list = []
-  def __init__(self, first_name, last_name, phone_number, email_address, password):
+  def __init__(self, first_name, last_name, phone_number, email_address, userName, password):
 
     self.first_name = first_name
     self.last_name = last_name
     self.phone_number = phone_number
     self.email_address = email_address
+    self.userName = userName
     self.password = password
 
   def save_userDetails(self):
--- user_test.py
+++ user_test.py
@@ -4,18 +4,29 @@ from credentials import Credentials
 class TestUser(unittest.TestCase):
 
   def setUp(self):
-    self.new_user = User("Paul", "Wamaria", "0780404626", "helloemryon@gmail.com","leejones1")
+    self.new_user = User("Paul", "Wamaria", "0780404626", "helloemryon@gmail.com", "Paulwamaria", "leejones1")
 
   def test_init(self):
     self.assertEqual(self.new_user.first_name,"Paul")
     self.assertEqual(self.new_user.last_name,"Wamaria")
     self.assertEqual(self.new_user.phone_number,"0780404626")
     self.assertEqual(self.new_user.email_address,"helloemryon@gmail.com")
+    self.assertEqual(self.new_user.userName, "Paulwamaria")
     self.assertEqual(self.new_user.password, "leejones1")
 
   def test_save_userDetails(self):
     self.new_user.save_userDetails()
     self.assertEqual(len(User.user_list),1)
+
+  def tearDown(self):
+    User.user_list = []
+
+  def test_save_multiple_userDetails(self):
+    self.new_userDetails.save_userDetails()
+    test_user = User("test","user","0780404626","helloemryon@gmail.com","Pwamaria","leej1")
+
+    test_user.save_user()
+    self.assertEqual(len(User.user_list),2)
 
 
 # testing credentials
