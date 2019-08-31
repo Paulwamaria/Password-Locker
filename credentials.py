466e649b26b4ac6f6231436f791e9244f1a6fa6d
Paulwamaria <paulwamaria@gmail.com>
2019-08-31 10:03:53 +0300

    Added a function to check if the entered detail; username and password matches any user in the users_list

--- credentials.py
+++ credentials.py
@@ -1,8 +1,27 @@
+from user import User
 class Credentials:
-  credentials_list = []
-  def __init__(self, account_type,registration_name, username, registration_email, account_password):
-    self.account_type = account_type
-    self.registration_name = registration_name
-    self.username = username
-    self.registration_email = registration_email
-    self.account_password = account_password
+
+    '''
+    A class to hold the user's credentials, genrate passwords and also store the information
+    '''
+    
+    credentials_list = []
+    def __init__(self, account_type,registration_name, username, registration_email, account_password):
+        self.account_type = account_type
+        self.registration_name = registration_name
+        self.username = username
+        self.registration_email = registration_email
+        self.account_password = account_password
+
+@classmethod
+def check_if_userExist(cls,username,password):
+
+    '''
+    A method to check if the user with the entered credentials exist
+    '''
+
+    current_user = " "
+    for user in User.users_list:
+        if user.username == userName and user.password == password:
+            current_user = user.first_name
+    return current_user
--- user_test.py
+++ user_test.py
@@ -3,43 +3,57 @@ from user import User
 from credentials import Credentials
 class TestUser(unittest.TestCase):
 
-  def setUp(self):
-    self.new_user = User("Paul", "Wamaria", "0780404626", "helloemryon@gmail.com", "Paulwamaria", "leejones1")
+    def setUp(self):
+        self.new_user = User("Paul", "Wamaria", "0780404626", "helloemryon@gmail.com", "Paulwamaria", "leejones1")
 
-  def test_init(self):
-    self.assertEqual(self.new_user.first_name,"Paul")
-    self.assertEqual(self.new_user.last_name,"Wamaria")
-    self.assertEqual(self.new_user.phone_number,"0780404626")
-    self.assertEqual(self.new_user.email_address,"helloemryon@gmail.com")
-    self.assertEqual(self.new_user.userName, "Paulwamaria")
-    self.assertEqual(self.new_user.password, "leejones1")
+    def test_init(self):
+        self.assertEqual(self.new_user.first_name,"Paul")
+        self.assertEqual(self.new_user.last_name,"Wamaria")
+        self.assertEqual(self.new_user.phone_number,"0780404626")
+        self.assertEqual(self.new_user.email_address,"helloemryon@gmail.com")
+        self.assertEqual(self.new_user.username, "Paulwamaria")
+        self.assertEqual(self.new_user.password, "leejones1")
 
-  def test_save_userDetails(self):
-    self.new_user.save_userDetails()
-    self.assertEqual(len(User.user_list),1)
 
-  def tearDown(self):
-    User.user_list = []
 
-  def test_save_multiple_userDetails(self):
-    self.new_userDetails.save_userDetails()
-    test_user = User("test","user","0780404626","helloemryon@gmail.com","Pwamaria","leej1")
+    def tearDown(self):
+        User.users_list = []
 
-    test_user.save_user()
-    self.assertEqual(len(User.user_list),2)
+    def test_save_multiple_userDetails(self):
+        self.new_user.save_userDetails()
+        test_user = User("test","user","0780404626","helloemryon@gmail.com","Pwamaria","leejones1")
+
+        test_user.save_userDetails()
+        self.assertEqual(len(User.users_list),2)
 
 
 # testing credentials
 class TestCredentials(unittest.TestCase):
-  def setUp(self):
-    self.new_credentials = Credentials("twitter","Just Paul","@Paulenigmatico","paulwamaria@gmail.com","lee1")
+    def setUp(self):
+        self.new_credentials = Credentials("twitter", "Just Paul","@Paulenigmatico", "paulwamaria@gmail.com","leejones1")
 
-  def test_init(self):
-    self.assertEqual(self.new_credentials.account_type,"twitter")
-    self.assertEqual(self.new_credentials.registration_name,"Just Paul")
-    self.assertEqual(self.new_credentials.username,"@Paulenigmatico")
-    self.assertEqual(self.new_credentials.registration_email,"paulwamaria@gmail.com")
-    self.assertEqual(self.new_credentials.account_password, "lee1")
+    def test_init(self):
+        self.assertEqual(self.new_credentials.account_type,"twitter")
+        self.assertEqual(self.new_credentials.registration_name,"Just Paul")
+        self.assertEqual(self.new_credentials.username,"@Paulenigmatico")
+        self.assertEqual(self.new_credentials.registration_email,"paulwamaria@gmail.com")
+        self.assertEqual(self.new_credentials.account_password, "leejones1")
+
+    def test_check_if_userExist(self):
+        '''
+        A test function to ensure the working of the check if userExist function.
+        '''
+        self.new_user = User("test","user","0780404626","helloemryon@gmail.com","Pwamaria","leejones1")
+        self.new_user.save_userDetails()
+
+        user2 = User("test","user","0780404626","helloemryon@gmail.com","Pwamaria","leejones1")
+
+        for user in User.users_list:
+            if user.username == user2.username and user.password == user2.password:
+                current_user = user.first_name
+        return current_user
+
+
 
 if __name__=='__main__':
   unittest.main()
