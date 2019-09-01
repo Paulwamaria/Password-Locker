ad5a4ee47d1bf2bf1b92cb0f2c898fe4354b24b9
Paulwamaria <paulwamaria@gmail.com>
2019-09-01 06:18:59 +0300

    Add a function to generate passwords

--- Pword_Locker.py
+++ Pword_Locker.py
@@ -8,10 +8,13 @@ from credentials import User
 
 #Create a new user
 def create_user(fname, lname, pnumber, eaddress, uname, pword):
+    '''
+    A funtcion to create a new user.
+    '''
     new_user = User(fname, lname, pnumber, eaddress, uname, pword)
     return new_user
 
-#save contacts
+#save user
 def save_userDetails(contact):
     '''
     a funtion to save user details
@@ -19,6 +22,22 @@ def save_userDetails(contact):
     User.save_userDetails()
 
 
-#Delete contact
+#delete user
 def delete_contacts(contact):
+    '''
+    A funtcion to delete a given user.
+    '''
     contact.delete_contact()
+
+
+def check_if_userExist(uname, pword):
+    '''
+    A function to return the user associated with the give password and username.
+    '''
+    return Credentials.check_if_userExist(uname, pword)
+
+def generate_pword(pword_length):
+    '''
+    A funtion to generate password, combining random letters and digits
+    '''
+    return Credentials.generate_password(pword_length)
